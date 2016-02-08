6858917e65553437fb9dffb48e0214cb51f3b091
louiefloresca89 <louie.floresca@outsourced.ph>
2016-02-08 14:23:22 +0800

    Update EmojiParser.java

--- src/main/java/com/vdurmont/emoji/EmojiParser.java
+++ src/main/java/com/vdurmont/emoji/EmojiParser.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.*;
 
 /**
  * Provides methods to parse strings with emojis.
@@ -93,9 +92,6 @@ public class EmojiParser {
                         replacement += candidate.fitzpatrick.unicode;
                     }
                     result = result.replace(":" + candidate.fullString + ":", replacement);
-                    
-                    System.out.println("EmojiParser parseToUnicode = " + candidate.fullString  + " = " replacement);
-                    
                 }
             }
         }
