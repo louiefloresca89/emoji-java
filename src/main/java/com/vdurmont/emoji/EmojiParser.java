c51f16f6034014abd83df6cf59865f27ce839c69
louiefloresca89 <louie.floresca@outsourced.ph>
2016-02-08 14:18:29 +0800

    Update EmojiParser.java

--- src/main/java/com/vdurmont/emoji/EmojiParser.java
+++ src/main/java/com/vdurmont/emoji/EmojiParser.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.*;
 
 /**
  * Provides methods to parse strings with emojis.
